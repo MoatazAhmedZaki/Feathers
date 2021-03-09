aab60e1038b87e8243a9cf34a39db6255974f9af
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-09 17:03:08 +0200

    Updates

---
+++ precache-manifest.c74eaaa6813b16d7dfe76a487c8b1356.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7563803f663f7d9b40e0a8aad89ae017",
+    "url": "/Feathers/index.html"
+  },
+  {
+    "revision": "b3a89c16f7288d216ff9",
+    "url": "/Feathers/static/css/2.cd3963c2.chunk.css"
+  },
+  {
+    "revision": "70dd10e12a4b7a26dc8f",
+    "url": "/Feathers/static/css/main.11c39f44.chunk.css"
+  },
+  {
+    "revision": "b3a89c16f7288d216ff9",
+    "url": "/Feathers/static/js/2.e251f9d7.chunk.js"
+  },
+  {
+    "revision": "70dd10e12a4b7a26dc8f",
+    "url": "/Feathers/static/js/main.d5d9020b.chunk.js"
+  },
+  {
+    "revision": "6c386b26f4d3ec36d238",
+    "url": "/Feathers/static/js/runtime-main.3e94be8b.js"
+  },
+  {
+    "revision": "afe59093ea9cb338717f748c3ae6350e",
+    "url": "/Feathers/static/media/contact-header.afe59093.png"
+  },
+  {
+    "revision": "9ee3dfa7a76540cba430ca7fb288341c",
+    "url": "/Feathers/static/media/homehader2.9ee3dfa7.jpg"
+  },
+  {
+    "revision": "1021b149e68793bc972c292ff2efad4b",
+    "url": "/Feathers/static/media/homerHeader.1021b149.bmp"
+  },
+  {
+    "revision": "1387591da01f991d6eed572f0ff4159e",
+    "url": "/Feathers/static/media/item.1387591d.png"
+  },
+  {
+    "revision": "1001c503e9294c30ff80c71c18f2c698",
+    "url": "/Feathers/static/media/item1.1001c503.bmp"
+  },
+  {
+    "revision": "15e0c37922eb9a0513da291b6b332830",
+    "url": "/Feathers/static/media/item2.15e0c379.bmp"
+  },
+  {
+    "revision": "ac3d4e769215a2b53a2df1fc70123634",
+    "url": "/Feathers/static/media/item3.ac3d4e76.bmp"
+  },
+  {
+    "revision": "986f72b4ddd4870b20bc35513fc453e6",
+    "url": "/Feathers/static/media/item4.986f72b4.bmp"
+  },
+  {
+    "revision": "0ba7613b851447d369dfb605490808d1",
+    "url": "/Feathers/static/media/item5.0ba7613b.bmp"
+  },
+  {
+    "revision": "840c36fa848c84d49e80907982575d99",
+    "url": "/Feathers/static/media/itunes-icon.840c36fa.png"
+  },
+  {
+    "revision": "fa120b8006c73a4e57a9e578058de9d9",
+    "url": "/Feathers/static/media/khalid.fa120b80.png"
+  },
+  {
+    "revision": "421eb312ef5e305dbe538e452ed5fde8",
+    "url": "/Feathers/static/media/logo.421eb312.svg"
+  },
+  {
+    "revision": "95085fe50e575c98d97ac15fb8f0623d",
+    "url": "/Feathers/static/media/pattern_dark.95085fe5.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Feathers/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Feathers/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Feathers/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Feathers/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c5eb262d775c3209ccb87a9ecf7b08df",
+    "url": "/Feathers/static/media/wallet1.c5eb262d.jpg"
+  },
+  {
+    "revision": "4a0a14496b4efc0ebfd4da8f26dfa9b6",
+    "url": "/Feathers/static/media/wallet2.4a0a1449.jpg"
+  },
+  {
+    "revision": "7db43056ae5d01f33bfc198916dd33cb",
+    "url": "/Feathers/static/media/wallet3.7db43056.jpg"
+  }
+]);
